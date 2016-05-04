86b1e288568caf798b1341d0ba9376f3ee518f49
Sriram Thiagarajan <sri.sjc@gmail.com>
2016-05-04 20:11:11 +0530

    DB schema for bookmarks

---
+++ src/db.js
@@ -0,0 +1,22 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+exports.connect = function(url){
+	mongoose.connect(url, function(err){
+		if(err)
+			throw err;
+	});
+	return exports;
+}
+
+// Bookmark schema
+var BookmarkSchema = new Schema({
+	title: String,
+	url: String,
+	folder: String
+});
+
+exports.db = mongoose;
+exports.models = {
+	Bookmark: mongoose.model('Bookmark', BookmarkSchema)
+};
