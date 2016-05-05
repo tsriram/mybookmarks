22f407f20e380d685e63250466ff086f8a9848fd
Sriram Thiagarajan <sri.sjc@gmail.com>
2016-05-05 20:22:16 +0530

    Some basic styling

--- assets/components/bookmark_view.jsx
+++ assets/components/bookmark_view.jsx
@@ -9,10 +9,11 @@ export class BookmarkView extends React.Component {
 
     render() {
         return(
-        	<div>
-        		<h2>{this.props.title}</h2>
-        		<span>{this.props.url}</span>
-        		<span>{this.props.folder}</span>
+        	<div className='row'>
+        		<h4>{this.props.title}</h4>
+        		<a href={this.props.url} target='_blank'>{this.props.url}</a>
+        		<div>{this.props.folder}</div>
+        		<hr/>
         	</div>
         )
     }
